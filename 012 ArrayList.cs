da3fcb3b78029c84985f638516a8030a9ec6d9d2
KevinSmish <KevinSmish@users.noreply.github.com>
2017-11-25 19:47:15 +0300

    Update 012 ArrayList.cs

--- 012 ArrayList.cs
+++ 012 ArrayList.cs
@@ -25,10 +25,60 @@ namespace ConsoleApp1
             System.Console.WriteLine(new String('-',30));
             arrayList.Remove(22);
 
+            // Добавление в набор групп элементов используя метод AddRange.
+            var anArray = new [] { "more", "or", "less" };
+            arrayList.AddRange(anArray);
+
+            var anotherArray = new[] { new object(), new ArrayList() };
+            arrayList.AddRange(anotherArray);
+
+            // Вставка элементов в заданное положение используя метод Insert.
+            arrayList.Insert(3, "Hey All");
+
+            // Вставка элементов в заданное положение используя метод InsertRange.
+            var moreString = new[] { "goodnight", "see ya" };
+            arrayList.InsertRange(4, moreString);
+
+            // Вставка элементов в заданное положение используя индексатор.
+            // (!) При использовании индексатора элемент не вставляется в набор, а перезаписывается прежний объект, бывший в этом элементе.
+            arrayList[2] = "Ali (Second)";
+
+            foreach (var item in arrayList)
+            {
+	            // Never do this!!!
+                // arrayList.Remove(item);
+
+                System.Console.WriteLine(item);
+            }
+
+            // Удаление из набора одиночных элементов используя метод Remove.
+            arrayList.Add("Hello");			// Добавим второе Hello
+            arrayList.Remove("Hello");		// Удалим первое Hello
+       
+            // Удаление из набора одиночных элементов с заданным индексом используя метод RemoveAt.
+            arrayList.RemoveAt(0);
+
+            // Удаление из набора, группы элементов с заданным диапазоном используя метод RemoveRange.
+            arrayList.RemoveRange(0, 4);
+
+            // Другие методы для добавления и удаления элементов набора - Contains, IndexOf, Clear.
+            string myString = "Hello";
+
+            if (arrayList.Contains(myString))
+            {
+                int index = arrayList.IndexOf(myString);
+                arrayList.RemoveAt(index);
+            }
+
+            System.Console.WriteLine(new String('-',30));
             foreach (var item in arrayList)
             {
                 System.Console.WriteLine(item);
             }
+
+			arrayList.Clear();
+            System.Console.WriteLine(new String('-',30));
+            System.Console.WriteLine("Count:{0}",arrayList.Count);
 
             System.Console.ReadKey();
         }
