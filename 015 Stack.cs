dee9ae7d87493f6d8b8b84ec7b5ae2b950e26351
KevinSmish <KevinSmish@users.noreply.github.com>
2017-11-25 19:52:53 +0300

    Update 015 Stack.cs

--- 015 Stack.cs
+++ 015 Stack.cs
@@ -9,19 +9,26 @@ namespace ConsoleApp1
         {
             Stack history = new Stack();
 
-            history.Push("One");
+            history.Push("One");									// Push() - добавляет элемент в конец стека.
             history.Push("Two");
             history.Push("Three");
             history.Push("Four");
 
             Console.WriteLine("Total elements in stack are {0}", history.Count);
 
-            Console.WriteLine("Pop (last): {0}", history.Pop());
-            Console.WriteLine("Peek (last): {0}", history.Peek());
+            Console.WriteLine("Pop (last): {0}", history.Pop());	// Pop() - возвращает первый элемент стека, удаляя его.
+            Console.WriteLine("Peek (last): {0}", history.Peek());	// Peek() - возвращает элемент с вершины стека, не удаляя его.
 
             Console.WriteLine(new String('-', 30));
             foreach (var item in history)
                 Console.WriteLine("Element: {0}", item);
+
+            Console.WriteLine(new String('-', 30));
+            // Count - возвращает количество элементов в стеке.
+            while (history.Count > 0)
+            {
+                Console.WriteLine(history.Pop());
+            }
 
             System.Console.ReadKey();
         }
