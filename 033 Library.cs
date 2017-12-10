28d815f28423b25018fcd8830131e9ac3fee0563
Kevin Smith <KevinSmish@users.noreply.github.com>
2017-12-10 16:38:39 +0300

    Create 033 Library.cs

---
+++ 033 Library.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TestDll
+{
+	public class MyLibrary
+	{
+		public static void func()
+		{
+			System.Console.WriteLine("Вызов функции func() из библиотеки.");
+		}	
+	}
+}
